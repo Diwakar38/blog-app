d9069b7c5d57abc8f6182f9b1e8439ee89c29fea
Diwakar <diwakar.arya1999@gmail.com>
2023-11-02 16:39:53 +0530

    final changes

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect, useState } from "react";
-import Edit from "../img/edit.png";
-import Delete from "../img/delete.png";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import Menu from "../components/Menu";
-import axios from "axios";
-import moment from "moment";
-import { useContext } from "react";
-import { AuthContext } from "../context/authContext";
-import DOMPurify from "dompurify";
-
+import React, { useContext, useState, useEffect } from 'react'
+import Edit from "../img/edit.png"
+import Delete from "../img/delete.png"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import Menu from "../components/Menu"
+import moment from "moment"
+import axios from "axios"
+import { AuthContext } from '../context/authContext'
 const Single = () => {
-  const [post, setPost] = useState({});
+	const [post, setPost] = useState([])
+	const navigate = useNavigate()
+	const location = useLocation();
 
-  const location = useLocation();
-  const navigate = useNavigate();
+	const postId = location.pathname.split("/")[2]
+	// console.log(postId)
+	const { currentUser } = useContext(AuthContext)
 
-  const postId = location.pathname.split("/")[2];
-
-  const { currentUser } = useContext(AuthContext);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -43,42 +40,38 @@ const Single = () => {
 		}
 	}
 
-  // const getText = (html) =>{
-  //   const doc = new DOMParser().parseFromString(html, "text/html")
-  //   return doc.body.textContent
-  // }
 
-  return (
-    <div className="single">
-      <div className="content">
-        <img src={`../upload/${post?.img}`} alt="Image not found" />
-        <div className="user">
-          {post.userImg && <img
-            src={post.userImg}
-            alt=""
-          />}
-          <div className="info">
-            <span>{post.username}</span>
-            <p>Posted {moment(post.date).fromNow()}</p>
-          </div>
-          {currentUser.username === post.username && (
-            <div className="edit">
-              <Link to={`/write?edit=2`} state={post}>
-                <img src={Edit} alt="Edit" />
-              </Link>
-              <img onClick={handleDelete} src={Delete} alt="Del" />
-            </div>
-          )}
-        </div>
-        <h1>{post.title}</h1>
-        <p
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(post.desc),
-          }}
-        ></p>      </div>
-      <Menu cat={post.cat}/>
-    </div>
-  );
-};
+	const getText = (html) => {
+		const doc = new DOMParser().parseFromString(html, "text/html")
+		return doc.body.textContent
+	}
 
-export default Single;
+
+	return (
+		<div className='single'>
+			<div className="content">
+				<img src={`../upload/${post?.img}`} alt="userImage" />
+				<div className="user">
+					{post.userImg && <img src={post.userImg} alt="" />}
+					<div className="info">
+						<span>{post.username}</span>
+						<p>Posted {moment(post.date).fromNow()}</p>
+					</div>
+					{currentUser && currentUser.username === post.username && (
+						<div className="edit">
+							<Link to={`/write?edit=2`} state={post}>
+								<img src={Edit} alt="edit_icon" />
+							</Link>
+							<img onClick={handleDelete} src={Delete} alt="delete_icon" />
+						</div>
+					)}
+				</div>
+				<h1>{post.title}</h1>
+				{getText(post.desc)}
+			</div>
+			<Menu cat={post.cat} />
+		</div>
+	)
+}
+
+export default Single

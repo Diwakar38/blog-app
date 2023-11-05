dfccfc16aefc30bd8fd141aca72bc6a2f93e1978
Diwakar <diwakar.arya1999@gmail.com>
2023-11-05 11:40:14 +0530

    url changes

--- client/src/Back.js
+++ client/src/Back.js
@@ -1,2 +1,2 @@
-export const URL = "https://blogapp-backend-iu5o.onrender.com";
-// export const URL = "http://localhost:8800";
+export default URL = "https://blogapp-backend-iu5o.onrender.com";
+// export default URL = "http://localhost:8800";
